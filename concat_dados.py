08c75e790e24eb6edefcecbaa45ae476fb7e485e
Emanuel Bezerra <emanuelsbzra@gmail.com>
2024-07-19 16:04:59 +0000

    adicionando o primeiro algoritmo

---
+++ concat_dados.py
@@ -0,0 +1,11 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma unica string
+
+# Recebe as duas entradas do usuário
+entrada1 = input("Digite a primeira entrada: ")
+entrada2 = input("Digite a segunda entrada: ")
+
+# Concatena as duas entradas em uma única string
+resultado = entrada1 + " " + entrada2
+
+# Exibe o resultado
+print("A string concatenada é:", resultado)
